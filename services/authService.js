d3e7c92c365c2e31da5da3d78a25396f5dd55a8b
idsnext <aruna.wickramasinghe@idsnext.com>
2020-04-05 01:35:58 +0530

    update login method

--- services/authService.js
+++ services/authService.js
@@ -5,107 +5,116 @@ const Profile = require("../models/profileModel");
 const atob = require("atob");
 const btoa = require("btoa");
 
-module.exports.ValidateUser = async user => {
+module.exports.ValidateUser = async (user) => {
   var user = await User.findOne({
     where: {
       username: atob(user.networkStatus1),
       password: atob(user.networkStatus2),
       isActive: true,
-      isDeleted: false
-    }
+      isDeleted: false,
+    },
   });
 
   if (user) {
-    var passObj = {
-      userId : user.id,
-      username : user.username
+    if (user.userType == "HOUSEHOLD") {
+      var profile = await Profile.findOne({
+        where: {
+          userId: user.id,
+          isActive: true,
+          isDeleted: false,
+        },
+      });
+
+      if (profile) {
+        var passObj = {
+          userId: user.id,
+          username: user.username,
+          isProfileCompleted: true,
+        };
+        var obj = {
+          Code: 0,
+          Message: "Success",
+          Data: passObj,
+        };
+
+        return obj;
+      } else {
+        var passObj = {
+          userId: user.id,
+          username: user.username,
+          isProfileCompleted: false,
+        };
+        var obj = {
+          Code: 0,
+          Message: "Success",
+          Data: passObj,
+        };
+
+        return obj;
+      }
     }
-    var obj = {
-      Code: 0,
-      Message: "Success",
-      Data: passObj
-    };
-  
-    return obj;
-    // if (user.userType == "Customer") {
-    //   var profile = await Profile.findOne({
-    //     where: {
-    //       userId: user.id,
-    //       isActive: true,
-    //       isDeleted: false
-    //     }
-    //   });
 
-    //   if (profile) {
-    //     var obj = {
-    //       Code: 0,
-    //       Message: "Success",
-    //       Data: profile
-    //     };
+    if (user.userType == "BUSINESS") {
+      var profile = await Company.findOne({
+        where: {
+          userId: user.id,
+          isActive: true,
+          isDeleted: false,
+        },
+      });
 
-    //     return obj;
-    //   } else {
-    //     var obj = {
-    //       Code: 1,
-    //       Message: "User not found!",
-    //       Data: null
-    //     };
+      if (profile) {
+        var passObj = {
+          userId: user.id,
+          username: user.username,
+          isProfileCompleted: true,
+        };
+        var obj = {
+          Code: 0,
+          Message: "Success",
+          Data: passObj,
+        };
 
-    //     return obj;
-    //   }
-    // }
+        return obj;
+      } else {
+        var passObj = {
+          userId: user.id,
+          username: user.username,
+          isProfileCompleted: false,
+        };
+        var obj = {
+          Code: 0,
+          Message: "Success",
+          Data: passObj,
+        };
 
-    // if (user.userType == "Company") {
-    //   var profile = await Company.findOne({
-    //     where: {
-    //       userId: user.id,
-    //       isActive: true,
-    //       isDeleted: false
-    //     }
-    //   });
-
-    //   if (profile) {
-    //     var obj = {
-    //       Code: 0,
-    //       Message: "Success",
-    //       Data: profile
-    //     };
-
-    //     return obj;
-    //   } else {
-    //     var obj = {
-    //       Code: 1,
-    //       Message: "User not found!",
-    //       Data: null
-    //     };
-
-    //     return obj;
-    //   }
-    // }
+        return obj;
+      }
+    }
   }
 
   var obj = {
     Code: 1,
     Message: "Failed",
-    Data: null
+    Data: null,
   };
 
   return obj;
 };
 
-module.exports.CreateUser = async user => {
+module.exports.CreateUser = async (user) => {
   let username = atob(user.basicInfo.networkStatus1);
   var count = await User.count({
     where: {
-      username: username
-    }
+      username: username,
+    },
   });
 
   if (count > 0) {
     var obj = {
       Code: 1,
       Message: "Username already exist!",
-      Data: null
+      Data: null,
     };
 
     return obj;
@@ -113,14 +122,14 @@ module.exports.CreateUser = async user => {
     console.log(user);
     user.basicInfo.username = atob(user.basicInfo.networkStatus1);
     user.basicInfo.password = atob(user.basicInfo.networkStatus2);
-     
+
     var createdUser = await User.create(user.basicInfo);
     console.log(createdUser);
     if (createdUser) {
       var obj = {
         Code: 0,
         Message: "Please login to continue!",
-        Data: null
+        Data: null,
       };
 
       return obj;
@@ -128,7 +137,7 @@ module.exports.CreateUser = async user => {
       var obj = {
         Code: 1,
         Message: "Something went wrong!",
-        Data: null
+        Data: null,
       };
 
       return obj;
