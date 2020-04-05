2f9c137fcde5a7d39cdcdb590a6d4ef80857bd7a
idsnext <aruna.wickramasinghe@idsnext.com>
2020-04-05 10:19:58 +0530

    add jwt

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
+const jwt = require('jsonwebtoken');
 
 
 var indexRouter = require('./routes/index');
